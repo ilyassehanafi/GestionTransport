7a8d278e8ca1f544caf370a1b61492028d1d1134
ilyassehanafi <ilyassehanafi@gmail.com>
2022-02-04 11:21:31 +0100

    update charts

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { Router } from '@angular/router';
 import { AuthentificationService } from '../authentification.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -11,7 +12,9 @@ import { AuthentificationService } from '../authentification.service';
 })
 export class ChartsComponent implements OnInit {
 
-  constructor(private route :Router, private authentificationService: AuthentificationService) {
+  constructor(private route :Router, private authentificationService: AuthentificationService,
+    private modalService: NgbModal
+    ) {
   }
 
   ngOnInit(): void {
@@ -25,29 +28,42 @@ export class ChartsComponent implements OnInit {
       text: 'Modelisation du réseau'
     },
     tooltip: {
-      show:true
-    },
+      show:true,
+      type: 'line',
+    }
+    ,
     toolbox: {
       show: true,
       feature: {
+        myTool1: {
+          show: true,
+          title: 'Données',
+          icon:'image://https://img.icons8.com/ios/50/000000/read.png',
+          onclick: function (){
+
+          }
+      },
         dataZoom: {
           yAxisIndex: "none"
         },
         dataView: {
-          readOnly: false
+          readOnly: true,
+          show:true
         },
         magicType: {
-          type: ["line", "bar"]
+          type: ["line"]
         },
-        restore: {},
-        saveAsImage: {}
+        restore: {show:true},
+        saveAsImage: {},
       }
-    },
-    dataset:{
-
     },
     animationDurationUpdate: 1500,
     animationEasingUpdate: 'quinticInOut',
+    dataset:{
+      source:
+        [{product: 'Matcha Latte', count: 823, score: 95.8} ]
+
+    },
     series: [
       {
         type: 'graph',
@@ -66,236 +82,209 @@ export class ChartsComponent implements OnInit {
           {
             name: 'Node 1',
             x: 150,
-            y: 500
+            y: 500,
+            value: 188,
+
+            itemStyle: {
+                color: 'red'
+            }
           },
           {
             name: 'Node 2',
             x: 550,
-            y: 200
-          },
+            y: 200,
+            value: 315
+            },
           {
             name: 'Node 3',
             x: 900,
-            y: 50
+            y: 50,
+            value: 345,
+            itemStyle: {
+                color: 'red'
+            }
           },
           {
             name: 'Node 4',
             x: 1200,
-            y: 100
+            y: 100,
+            value: 297
           },
           {
             name: 'Node 5',
             x: 1500,
-            y: 200
+            y: 200,
+            value: 106
           },
           {
             name: 'Node 6',
             x: 1700,
-            y: 400
+            y: 400,
+            value: 167,
+            itemStyle: {
+                color: 'red'
+            }
           },
           {
             name: 'Node 7',
             x: 1400,
-            y: 600
+            y: 600,
+            value: 146
           },
           {
             name: 'Node 8',
             x: 1200,
-            y:720
+            y:720,
+            value: 160
           },
           {
             name: 'Node 9',
             x: 1000,
-            y: 900
+            y: 900,
+            value: 359
           },
           {
             name: 'Node 10',
             x: 1000,
-            y: 1200
+            y: 1200,
+            value: 337,
+            itemStyle: {
+                color: 'red'
+            }
           },
           {
             name: 'Node 11',
-            x: 700,
-            y: 1000
+            x: 500,
+            y: 800,
+            value: 140
           },
           {
             name: 'Node 12',
-            x: 400,
-            y: 800
-          },{
-            name: 'Node 13',
-            x: 900,
-            y: 250
-          },{
-            name: 'Node 14',
             x: 800,
-            y: 550
-          },{
-            name: 'Node 15',
-            x: 1200,
-            y: 450
+            y: 450,
+          },
+          {
+            name: 'Node 13',
+            x: 1000,
+            y: 250,
           }
         ],
         links: [
           {
             source: 'Node 1',
-            target: 'Node 2'
+            target: 'Node 2',
+            label:{
+              show:true,
+              position:'middle'
+            }
+
           },
           {
             source: 'Node 2',
-            target: 'Node 1'
+            target: 'Node 3',
+            label:{
+              show:true,
+              position:'middle'
+            }
           },
           {
-            source: 'Node 2',
-            target: 'Node 3'
+            source: 'Node 3',
+            target: 'Node 4',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 4',
+            target: 'Node 5',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 5',
+            target: 'Node 6',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 6',
+            target: 'Node 7',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 7',
+            target: 'Node 8',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 8',
+            target: 'Node 9',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 9',
+            target: 'Node 10',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 10',
+            target: 'Node 11',
+            label:{
+              show:true,
+              position:'middle'
+            }
+          },
+          {
+            source: 'Node 11',
+            target: 'Node 1',
+            label:{
+              show:true,
+              position:'middle'
+            }
           },{
-            source: 'Node 3',
-            target: 'Node 2'
-          },
-          {
-            source: 'Node 3',
-            target: 'Node 4'
-          },
-          {
-            source: 'Node 4',
-            target: 'Node 3'
-          },
-          {
-            source: 'Node 4',
-            target: 'Node 5'
-          },
-          {
-            source: 'Node 5',
-            target: 'Node 4'
-          },
-          {
-            source: 'Node 8',
-            target: 'Node 9'
-          },
-          {
-            source: 'Node 9',
-            target: 'Node 8'
-          },
-          {
-            source: 'Node 5',
-            target: 'Node 6'
-          },
-          {
-            source: 'Node 6',
-            target: 'Node 5'
-          },
-          {
-            source: 'Node 5',
-            target: 'Node 7'
-          },
-          {
-            source: 'Node 7',
-            target: 'Node 5'
-          },
-          {
-            source: 'Node 6',
-            target: 'Node 7'
-          },
-          {
-            source: 'Node 7',
-            target: 'Node 6'
-          },
-          {
-            source: 'Node 7',
-            target: 'Node 8'
-          },
-          {
-            source: 'Node 8',
-            target: 'Node 7'
-          },
-          {
-            source: 'Node 9',
-            target: 'Node 10'
-          },
-          {
-            source: 'Node 10',
-            target: 'Node 9'
-          },
-          {
-            source: 'Node 10',
-            target: 'Node 11'
-          }
-          ,
-          {
-            source: 'Node 11',
-            target: 'Node 10'
-          },
-          {
             source: 'Node 11',
             target: 'Node 12'
-          },
-          {
-            source: 'Node 12',
-            target: 'Node 11'
-          },
-          {
-            source: 'Node 12',
-            target: 'Node 1'
-          },
-          {
-            source: 'Node 1',
-            target: 'Node 12'
-          },
-          {
-            source: 'Node 11',
-            target: 'Node 14'
-          },
-          {
-            source: 'Node 14',
-            target: 'Node 11'
           },
           {
             source: 'Node 2',
-            target: 'Node 14'
+            target: 'Node 12'
           },
           {
-            source: 'Node 14',
-            target: 'Node 2'
-          },
-          {
-            source: 'Node 3',
+            source: 'Node 12',
             target: 'Node 13'
+          },
+          {
+            source: 'Node 13',
+            target: 'Node 9'
           },
           {
             source: 'Node 13',
             target: 'Node 3'
           },
           {
-            source: 'Node 13',
-            target: 'Node 15'
-          },
-          {
-            source: 'Node 15',
-            target: 'Node 13'
-          },
-          {
             source: 'Node 4',
-            target: 'Node 7'
-          },
-          {
-            source: 'Node 7',
-            target: 'Node 4'
-          },
-          {
-            source: 'Node 15',
             target: 'Node 8'
           },
           {
-            source: 'Node 8',
-            target: 'Node 15'
-          },
-          {
-            source: 'Node 13',
-            target: 'Node 14'
-          },
-          {
-            source: 'Node 14',
-            target: 'Node 13'
+            source: 'Node 5',
+            target: 'Node 7'
           },
         ],
         lineStyle: {
@@ -306,8 +295,6 @@ export class ChartsComponent implements OnInit {
       }
     ]
   };
-
-
 
 
   }
